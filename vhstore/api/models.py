7c6316d19b86759bcfe5992bde447f610ade1c2b
a1trope <bigtinyturtle@gmail.com>
2024-07-03 15:35:40 +0400

    update model

--- vhstore/api/models.py
+++ vhstore/api/models.py
@@ -6,7 +6,7 @@ class Cassette(models.Model):
     title = models.CharField(max_length=100)
     director = models.CharField(max_length=50)
     released = models.DateField(default=date.today)
-    quantity = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"\"{self.title}\" - {self.director} ({self.released.year})"
