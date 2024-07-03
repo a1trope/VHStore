b21f7336bcbf20750a7cba688e52cfdcac4f9e7c
a1trope <bigtinyturtle@gmail.com>
2024-07-03 12:23:38 +0400

    api work in progress

--- vhstore/api/models.py
+++ vhstore/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Cassette(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=50)
+    released = models.DateField(default=date.today)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"\"{self.title}\" - {self.director} ({self.released.year})"
